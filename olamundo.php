3d99391e87a5da02bea61b1122481fa88679f0c9
unknown <danielmoreicustodio1999@gmail.com>
2025-04-25 16:23:56 -0300

    Novas atualizações da aula de PHP

--- olamundo.php
+++ olamundo.php
@@ -1,6 +1,18 @@
 <?php
+/*
 echo "Olá mundo!";
-
 $idade = 19;
+echo $idade + 10;*/
 
-echo $idade + 10;
+/*$soma = 2 + 2;
+$multiplicacao = 3*3;
+$divisao = 6/26.600;
+$subtracao = 9-4;
+$maior = 11;*/
+
+/*$nome = "Daniel";*/
+
+/*$verdadeiro = true;
+$falso = false;*/
+
+echo gettype($falso);
